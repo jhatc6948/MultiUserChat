55e62a37cbcb2e5ef7389f9c1000fd68911ac5db
jhatc6948 <jhatc6948@westada.org>
2019-05-09 12:52:39 -0600

    added direct messaging between clients and added group messages where you can join and leave groups

--- ChatServer/src/com/muc/ServerWorker.java
+++ ChatServer/src/com/muc/ServerWorker.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -13,6 +14,7 @@ public class ServerWorker extends Thread {
     private final Server server;
     private String login = null;
     private OutputStream outputStream;
+    private HashSet<String> topicSet = new HashSet<>();
 
     public ServerWorker(Server server, Socket clientSocket) {
         this.server = server;
@@ -40,10 +42,18 @@ public class ServerWorker extends Thread {
             String[] tokens = StringUtils.split(line);     //String[] tokens Line.split(" ");
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
-                if ("quit".equalsIgnoreCase(line)) {
+                if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(cmd)) {
+                    handleLogoff();
                     break;
-                }else if ("login".equalsIgnoreCase(cmd)){
+                }else if ("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
+                }else if ("msg".equalsIgnoreCase(cmd)) {
+                    String[] tokensMsg = StringUtils.split(line, null, 3);
+                    handleMessage(tokensMsg);
+                }else if ("join".equalsIgnoreCase(cmd)) {
+                    handleJoin(tokens);
+                }else if ("leave".equalsIgnoreCase(cmd)){
+                    handleLeave(tokens);
                 }else{
                     String msg = "unknown" + cmd + "\n";
                     outputStream.write(msg.getBytes());
@@ -54,6 +64,64 @@ public class ServerWorker extends Thread {
         clientSocket.close();
     }
 
+    private void handleLeave(String[] tokens) {
+        if(tokens.length > 1){
+            String topic = tokens[1];
+            topicSet.remove(topic);
+
+        }
+    }
+
+
+    public boolean isMemberOfTopic(String topic){
+        return topicSet.contains(topic);
+    }
+
+    private void handleJoin(String[] tokens) {
+        if(tokens.length > 1){
+            String topic = tokens[1];
+            topicSet.add(topic);
+
+        }
+    }
+
+    private void handleMessage(String[] tokens) throws IOException {
+        String sendTo = tokens[1];
+        String body = tokens[2];
+
+        boolean isTopic = sendTo.charAt(0) == '#';
+
+        List<ServerWorker> workerList = server.getWorkerList();
+        for(ServerWorker worker : workerList) {
+            if(isTopic){
+                if (worker.isMemberOfTopic(sendTo)){
+                    String outMsg = "msg " + sendTo + ": " +  login + " " + body + "\n";
+                    worker.send(outMsg);
+                }
+            }else{
+                if(sendTo.equalsIgnoreCase(worker.getLogin())) {
+                    String outMsg = "msg " + login + " " + body + "\n";
+                    worker.send(outMsg);
+                }
+            }
+        }
+
+    }
+
+    private void handleLogoff() throws IOException {
+        server.removeWorker(this);
+        List<ServerWorker> workerList = server.getWorkerList();
+
+        //send  other online users current users status
+        String onlineMsg = "offline " + login + "\n";
+        for(ServerWorker worker : workerList){
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+
+            }
+        }
+        clientSocket.close();
+    }
     public String getLogin(){
         return login;
     }
@@ -63,16 +131,30 @@ public class ServerWorker extends Thread {
             String login = tokens[1];
             String password = tokens[2];
 
-            if ((login.equals("guest") && password.equals("guest")) || (login.equals("jim") && password.equals("jim"))){
+            if ((login.equals("perry") && password.equals("perry")) || (login.equals("josh") && password.equals("josh")) || (login.equals("kyle") && password.equals("kyle"))){
                 String msg = "ok login \n";
                 outputStream.write(msg.getBytes());
                 this.login = login;
-                System.out.println("user logged in successfully" + login);
+                System.out.println("user logged in successfully " + login);
 
-                String onlineMsg = "online " + login + "\n";
                 List<ServerWorker> workerList = server.getWorkerList();
+
+                //send current user all other online logins
                 for(ServerWorker worker : workerList) {
-                    worker.send(onlineMsg);
+                    if(worker.getLogin() != null){
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = "online " + worker.getLogin() + "\n";
+                            send(msg2);
+                        }
+                    }
+                }
+                //send  other online users current users status
+                String onlineMsg = "online " + login + "\n";
+                for(ServerWorker worker : workerList){
+                    if (!login.equals(worker.getLogin())) {
+                        worker.send(onlineMsg);
+
+                    }
                 }
             }else{
                 String msg = "error login \n";
@@ -82,6 +164,9 @@ public class ServerWorker extends Thread {
     }
 
     private void send(String msg) throws IOException {
-        outputStream.write(msg.getBytes());
+        if(login != null){
+            outputStream.write(msg.getBytes());
+        }
+
     }
 }
